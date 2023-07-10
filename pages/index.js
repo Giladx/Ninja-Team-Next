79110fbcb8219f54791a510e23f1681119d7e094
Gilad Levi <gilad.x@gmail.com>
2023-07-10 17:46:19 +0300

    Commit from TeleportHQ

--- pages/index.js
+++ pages/index.js
@@ -369,8 +369,8 @@ const Home = (props) => {
         </div>
     </div>
 
-    <!-- The script itself, loaded AFTER your root element. -->
-    <script src="https://unpkg.com/typeit@8.7.1/dist/index.umd.js"></script>
+    <!-- The script itself, loaded AFTER your root element. 
+    <script src="https://unpkg.com/typeit@8.7.1/dist/index.umd.js"></script>-->
     <script>
         new TypeIt("#descElement", {
                 speed: 25,
